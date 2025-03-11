e5fff33f68f10e047ea968efca33789d7fecf3dd
h80h <h80h525@gmail.com>
2025-03-12 03:43:09 +0800

    Optimize de algorithm of de calculator & debug haven't handle the decimal part

--- script.js
+++ script.js
@@ -16,10 +16,55 @@ function Calculator() {
 
 const powerCalc = new Calculator;
 const last = document.querySelector(".last");
-// const lastArr = last.texContent.split("");
-const lastContent = last.textContent;
-const operate = document.querySelector(".equal");
+const operands = document.querySelector(".operands");
+const operate = document.querySelector("#operate");
 const curr = document.querySelector(".curr");
+const ops = ["add", "subtract", "multiply", "divide", "operate"];
+let count = 0;
+
+operands.addEventListener("click", (e) => {
+  console.log(`count = ${count}`);
+  if(e.target.id !== "operate"){
+    if(count > 0) last.textContent = "";
+    if(ops.includes(e.target.id)){
+      if(count > 0) count --;
+      if(!curr.textContent.includes(" ")){
+        switch(e.target.id) {
+          case "add":
+            curr.textContent += " + ";
+            break;
+          case "subtract":
+            curr.textContent += " - ";
+            break;
+          case "multiply":
+            curr.textContent += " * ";
+            break;
+          case "divide":
+            curr.textContent += " / ";
+            break;
+        }
+      }
+    }else if(curr.textContent === "0"){
+      if(e.target.id === "0"){
+        ;
+      } else {
+        curr.textContent = e.target.id;
+      }
+    } else {
+      if(count > 0){
+        curr.textContent = e.target.id;
+        count --;
+      } else {
+      curr.textContent += e.target.id;
+      }
+    }
+  }
+})
+
 operate.addEventListener("click", () => {
-  return curr.textContent = powerCalc.calculate(lastContent);
+  if(!(curr.textContent.slice(-1) === " ") && curr.textContent.includes(" ")){
+    last.textContent = curr.textContent;
+    count ++;
+    return curr.textContent = powerCalc.calculate(curr.textContent);
+  }
 })
