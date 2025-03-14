7df53b5668c9731ddb1a419d43b6f3154cf9b572
h80h <h80h525@gmail.com>
2025-03-15 00:08:33 +0800

    Limit length of result for both decimal & big num also debug for decimal part

--- script.js
+++ script.js
@@ -10,7 +10,36 @@ function Calculator() {
     let op = splited[1];
     let a = +splited[0];
     let b = +splited[2];
-    return this.methods[op](a, b);
+    
+    let result = (this.methods[op](a, b)).toFixed(8);
+    console.log(`result = ${result}`);
+    let rounded = round(result);
+
+    function round(){
+      let decimalIndex = result.indexOf(".")
+      if(result >= 100000000){
+        let exponen = (+(result)).toExponential();
+        console.log(`exponen = ${exponen}`);
+        let exponenStr = exponen.toString();
+        let eIndex = exponenStr.indexOf("e");
+        let a = 9 - (exponenStr.length-eIndex) + 1;
+        
+        return (+exponen.slice(0, a)).toFixed(a - 3) + exponen.slice(eIndex);
+      } else if(decimalIndex != "-1"){
+        return (+result).toFixed(8-decimalIndex)
+      } else {
+        return result;
+      }
+    }
+
+    while(rounded.slice(-1) === "0"){
+      rounded = rounded.slice(0, -1);
+    }
+    if(rounded.slice(-1) === "."){
+      rounded = rounded.slice(0, -1);
+    }
+
+    return rounded;
   };
 }
 
@@ -20,14 +49,17 @@ const operands = document.querySelector(".operands");
 const operate = document.querySelector("#operate");
 const curr = document.querySelector(".curr");
 const ops = ["add", "subtract", "multiply", "divide", "operate"];
-let count = 0;
+let decimalCount = 1;
+let newCount = 0;
 
 operands.addEventListener("click", (e) => {
-  console.log(`count = ${count}`);
+  console.log(`newCount = ${newCount}`);
+  console.log(`decimalCount = ${decimalCount}`)
   if(e.target.id !== "operate"){
-    if(count > 0) last.textContent = "";
+    if(newCount > 0) last.textContent = "";
     if(ops.includes(e.target.id)){
-      if(count > 0) count --;
+      if(newCount > 0) newCount --;
+      if(decimalCount < 1) decimalCount ++;
       if(!curr.textContent.includes(" ")){
         switch(e.target.id) {
           case "add":
@@ -44,16 +76,30 @@ operands.addEventListener("click", (e) => {
             break;
         }
       }
-    }else if(curr.textContent === "0"){
+    } else if(e.target.id === "decimal"){
+      if(decimalCount > 0){
+        if(newCount > 0){
+          curr.textContent = "0."
+          newCount --;
+        } else if(curr.textContent.slice(-1) === " "){
+          curr.textContent += "0.";
+        } else {
+          curr.textContent += ".";
+        }
+        decimalCount --;
+      }
+    } else if(curr.textContent === "0"){
       if(e.target.id === "0"){
         ;
       } else {
         curr.textContent = e.target.id;
       }
     } else {
-      if(count > 0){
+      if(newCount > 0){
         curr.textContent = e.target.id;
-        count --;
+        newCount --;
+      } else if(curr.textContent.slice(-2) === " 0"){
+        curr.textContent = curr.textContent.slice(0, -1) + e.target.id;
       } else {
       curr.textContent += e.target.id;
       }
@@ -64,7 +110,8 @@ operands.addEventListener("click", (e) => {
 operate.addEventListener("click", () => {
   if(!(curr.textContent.slice(-1) === " ") && curr.textContent.includes(" ")){
     last.textContent = curr.textContent;
-    count ++;
+    newCount ++;
+    if(decimalCount < 1) decimalCount++;
     return curr.textContent = powerCalc.calculate(curr.textContent);
   }
 })
